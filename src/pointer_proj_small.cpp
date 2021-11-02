06387114b7126657839e1be428b6d1d7cf3bb640
JohnMadaj <jack@valu.net>
2021-11-01 21:28:24 -0400

    finished unfinished src code:

--- src/pointer_proj_small.cpp
+++ src/pointer_proj_small.cpp
@@ -8,10 +8,13 @@
 
 #include <iostream>
 #include <string.h>
-using namespace std;
+
 #include "../includes/constants.h"
 #include "../includes/stringmanip.h"
 #include "../includes/fileio.h"
+
+using namespace std;
+
 
 
 int main(int argc, char *argv[]) {	
@@ -21,7 +24,7 @@ int main(int argc, char *argv[]) {
 		return FAIL_WRONG_NUMBER_ARGS;
 	
 		//TODO
-	string in = argv[1];		//TODO THESE ARGUMENT INDICES COULD BE WRONG FIX THEM
+	string in = argv[1];
 	string out = argv[2];
 
 	string tag = argv[3];
@@ -32,28 +35,48 @@ int main(int argc, char *argv[]) {
 	char* replacement_char = new char[replacement.length() + 1 ];
 	copy(replacement.begin(), replacement.end(), replacement_char);
 
-	string src; //tODO FIX ME
-	char* src_char = new char[src.length() + 1];
-	copy(src.begin(), src.end(), src_char);
-	string contents; //this ones fine like this
-
-
-
-
+	string src;
 
 	//TODO can we read file? Fail if not
-	int read = readFile(in, contents);
+	int read = readFile(in, src);
 	if (read == FAIL_CANNOT_OPEN_FILE){
+		cout<<CANNOT_OPEN_FILE<<endl;
 		return read;
 	}
+
+	char* src_char = new char[src.length() + 1];
+		copy(src.begin(), src.end(), src_char);
+
 	//TODO how many tags found in file?
 	int numbTags = KP::findNumbOccurrences(src_char,  tag_char);
+
 	//TODO how much memory to allocate?
 	int required_memory = KP::amountOfMemoryToAllocateForNewString(src.length(), numbTags,
 			tag.length(), replacement.length());
+
 	//TODO allocate memory
+	char* modified_src_char = new char[required_memory + 1];
 	
 	//TODO replace original tag with new tags
+	int replace(src_char, modified_src_char, tag_char, replacement_char);
+	int write = writeFile(out, modified_src_char);
+	if (write == INVALID_NULL_PTR_DETECTED){
+		cout<<HELP_STRING2<<endl;
+		return write;
+	}
+
+
 	
 	//TODO deallocate any memory allocated
+	if (src_char)
+		delete src_char;
+	if (modified_src_char)
+		delete modified_src_char;
+	if (tag_char)
+		delete tag_char;
+	if (replacement_char)
+		delete replacement_char;
+
+	return SUCCESS;
+
 }
